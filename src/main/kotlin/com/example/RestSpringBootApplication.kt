6aa9d2f8966bd0abed452b450e22f4a99b0eef85
André Luis de Andrade <al.andrade256@gmail.com>
2023-02-13 06:41:45 -0300

    First steps in Kotlin with Spring Boot

---
+++ src/main/kotlin/com/example/RestSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestSpringBootApplication>(*args)
+}
