b625e360622937f05c8af3ace7ae6b13964cc10e
André Luis de Andrade <al.andrade256@gmail.com>
2023-03-14 07:02:13 -0300

    SpringSecurity: JWT

--- src/main/kotlin/com/example/RestSpringBootApplication.kt
+++ src/main/kotlin/com/example/RestSpringBootApplication.kt
@@ -8,4 +8,15 @@ class RestSpringBootApplication
 
 fun main(args: Array<String>) {
 	runApplication<RestSpringBootApplication>(*args)
+
+	/*
+	val encoders: MutableMap<String, PasswordEncoder> = HashMap()
+	encoders["pbkdf2"] = Pbkdf2PasswordEncoder()
+	val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
+	passwordEncoder.setDefaultPasswordEncoderForMatches(Pbkdf2PasswordEncoder())
+
+	val result = passwordEncoder.encode("foo-bar")
+	println("My hash $result")
+	*/
+
 }
--- src/main/kotlin/com/example/data/vo/v1/TokenVO.kt
+++ src/main/kotlin/com/example/data/vo/v1/TokenVO.kt
@@ -5,7 +5,7 @@ import java.util.*
 data class TokenVO (
     val username: String? = null,
     val authenticated: Boolean? = null,
-    val createad: Date? = null,
+    val created: Date? = null,
     val expiration: Date? = null,
     val accessToken: String? = null,
     val refreshToken: String? = null,
